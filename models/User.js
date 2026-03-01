04430a7a4766289f6ebe87c5c50f126524ec9c93
Anirudh <jeripothulaanirudh@gmail.com>
2026-03-01 23:32:57 +0530

    Fix: Restored accidentally removed coins schema param

--- models/User.js
+++ models/User.js
@@ -69,6 +69,7 @@ const UserSchema = new mongoose.Schema({
     equippedTitle: { type: String, default: "" }, // ID of the equipped title
     unlockedTitles: [{ type: String }], // IDs of unlocked titles
     claimedAchievements: [{ type: String }], // IDs of achievements claimed by user
+    coins: { type: Number, default: 0 }, // ** NEW VIRTUAL ECONOMY FIELD **
     xp: { type: Number, default: 0 }, // XP for Leveling System
     // Login Streak
     lastLoginDate: { type: Date, default: null },
